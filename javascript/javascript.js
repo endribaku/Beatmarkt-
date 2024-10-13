3fa4b30097f8966446d590c7b5eebd0f7b42f9d6
Endri Baku <176520897+endribaku@users.noreply.github.com>
2024-10-13 16:44:03 +0200

    feat: Create interactable track player

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,4 +1,4 @@
-function Track(image, title, track_length, bpm, price, tag1, tag2, tag3, genre) {
+function Track(image, title, track_length, bpm, price, tag1, tag2, tag3, genre, audio) {
     this.image = image;
     this.title = title;
     this.tracklength = track_length;
@@ -8,6 +8,7 @@ function Track(image, title, track_length, bpm, price, tag1, tag2, tag3, genre) 
     this.tag2 = tag2;
     this.tag3 = tag3;
     this.genre = genre;
+    this.audio = audio;
 }
 
 
@@ -40,24 +41,32 @@ img7.src = "../images/beat-images/jersey.webp";
 let img8 = new Image();
 img8.src = "../images/beat-images/smoke.webp";
 
+// audio files
+let audio1 = new Audio("../audio/sandman-buy-2-get-1-free_TK12131454.mp3.mp3");
+let audio2 = new Audio("../audio/nomads-buy-2-get-1-free_TK11752897.mp3.mp3")
+let audio3 = new Audio("../audio/let-me-down-buy-2-get-1-free_TK17729388.mp3.mp3")
+let audio4 = new Audio("../audio/long-talk-buy-2-get-1-free_TK16585534.mp3.mp3")
+let audio6 = new Audio("../audio/supervention-buy-2-get-1-free_TK15737160.mp3.mp3")
+let audio8 = new Audio("../audio/smoke-buy-2-get-1-free_11318083.mp3");
+
 
 let tracklistArray = [];
 
 
 let track1 = new Track(img1.src, "SANDMAN (BUY 2 GET 1 FREE)", "2:12", 
-    "96", "$19.95", "utopia", "asap rocky", undefined, "Trap");
+    "96", "$19.95", "utopia", "asap rocky", undefined, "Trap", audio1);
 tracklistArray.push(track1);
 
 let track2 = new Track(img2.src, "NOMADS (BUY 2 GET 1 FREE)", 
-    "3:06", "136", "$19.95", "kendrick lamar", "baby keem", "kodak black", "Trap");
+    "3:06", "136", "$19.95", "kendrick lamar", "baby keem", "kodak black", "Trap", audio2);
 tracklistArray.push(track2);
 
 let track3 = new Track(img3.src, "LET ME DOWN (BUY 2 GET 1 FREE)", "3:05", 
-    "166", "$19.95", "ty dolla sign", "vultures", "kanye west", "Hip-hop");
+    "166", "$19.95", "ty dolla sign", "vultures", "kanye west", "Hip-hop", audio3);
 tracklistArray.push(track3);
 
 let track4 = new Track(img4.src, "LONG TALK (BUY 2 GET 1 FREE)", "3:18", 
-    "145", "$19.95", "soul", "pray for paris", "westside gunn", "Hip-hop");
+    "145", "$19.95", "soul", "pray for paris", "westside gunn", "Hip-hop", audio4);
 tracklistArray.push(track4);
 
 let track5 = new Track(img5.src, "MISERY (BUY 2 GET 1 FREE", "3:22", 
@@ -65,7 +74,7 @@ let track5 = new Track(img5.src, "MISERY (BUY 2 GET 1 FREE", "3:22",
 tracklistArray.push(track5);
 
 let track6 = new Track(img6.src, "SUPERVENTION (BUY 2 GET 1 FREE)", "3:40", 
-    "125", "$19.95", "clams casino", "asap rocky", "ambient", "Lo-fi");
+    "125", "$19.95", "clams casino", "asap rocky", "ambient", "Lo-fi", audio6);
 tracklistArray.push(track6);
 
 let track7 = new Track(img7.src, "JERSEY (BUY 2 GET 1 FREE)", "3:47", "114", 
@@ -73,19 +82,22 @@ let track7 = new Track(img7.src, "JERSEY (BUY 2 GET 1 FREE)", "3:47", "114",
 tracklistArray.push(track7);
 
 let track8 = new Track(img8.src, "SMOKE (BUY 2 GET 1 FREE)", "3:08", "121", "$19.95", 
-    "travis scott", "travis scott utopia", "kanye west", "Trap");
+    "travis scott", "travis scott utopia", "kanye west", "Trap", audio8);
 tracklistArray.push(track8);
+
 
 
 
 
 // tracklist creator
 function createTracklist(tracklistArray) {
+    
     let tracklist = document.querySelector(".track-list");
     
 
     // Info for each track
     tracklistArray.forEach( (track) => {
+        
         let trackInfo = document.createElement("div");
         trackInfo.classList.add("track-info");
         tracklist.appendChild(trackInfo);
@@ -170,9 +182,25 @@ function createTracklist(tracklistArray) {
             textLength.classList.add("text-length");
             textLength.textContent = "Length";
             trackLength.appendChild(textLength);
+            
             let detailValue = document.createElement("div");
             detailValue.classList.add("detail-value");
-            detailValue.textContent = track.tracklength;
+            
+            let audioElement = track.audio;  // Assuming track.audio is an HTMLAudioElement
+        
+    // Listen for the 'loadedmetadata' event to make sure the duration is available
+            audioElement.addEventListener('loadedmetadata', () => {
+                let trackAudioLength = Math.floor(audioElement.duration);
+                let trackMinutes = Math.floor(trackAudioLength / 60);
+                let trackSeconds = trackAudioLength % 60;
+
+                 if (trackSeconds < 10) {
+                trackSeconds = "0" + trackSeconds;
+                }
+                detailValue.textContent = `${trackMinutes}:${trackSeconds}`;
+                console.log(`Track length: ${trackMinutes}:${trackSeconds}`);
+                });
+            
             trackLength.appendChild(detailValue);
             
             
@@ -200,6 +228,7 @@ function createTracklist(tracklistArray) {
             downloadImage.src = "../images/download-icon.png";
             downloadButton.appendChild(downloadImage);
             trackButtons.appendChild(downloadButton);
+            
             let shareButton = document.createElement("button");
             shareButton.type = "button";
             shareButton.classList.add("share-button");
@@ -207,6 +236,7 @@ function createTracklist(tracklistArray) {
             shareButtonImage.src = "../images/share icon.png";
             shareButton.appendChild(shareButtonImage);
             trackButtons.appendChild(shareButton);
+            
             let buyButton = document.createElement("button");
             buyButton.type = "button";
             buyButton.classList.add("buy-button");
@@ -219,6 +249,7 @@ function createTracklist(tracklistArray) {
             trackButtons.appendChild(buyButton);
             buyButton.appendChild(buyButtonImage);
             buyButton.appendChild(buyButtonPrice);
+            
 
             //Add Event Listener
             trackInfo.addEventListener("mouseenter", () => {
@@ -231,29 +262,163 @@ function createTracklist(tracklistArray) {
                 trackImage.classList.add("selectedTrackImage");
                 
                 trackImage.appendChild(playImage);
-               
-                trackInfo.addEventListener("mouseleave", () => {
-                    trackInfo.classList.remove("selectedTrack");
-                    trackImage.classList.remove("selectedTrackImage");
-                    trackImage.removeChild(playImage);
-                });
                 
+               
+                trackInfo.addEventListener("mouseleave", removeSelectedTrack);
+                
+                trackInfo.addEventListener("click", () => {
+                    if(document.querySelector(".track-playback") == undefined) {
+                        createTrackPlayer(track);
+                        trackInfo.classList.add("selectedTrack");
+                        trackInfo.removeEventListener("mouseleave", () => removeSelectedTrack);
+                    } else {
+                        let trackPlayback = document.querySelector(".track-playback");
+                        let body = document.querySelector("body");
+                        body.removeChild(trackPlayback);
 
-            })
+                        createTrackPlayer(track);
+                        trackInfo.classList.add("selectedTrack");
+                        trackInfo.removeEventListener("mouseleave", () => removeSelectedTrack);
 
-    })
-
-    
+                    }      
+                });                 
+            })        
+    }) 
 }
 
-createTracklist(tracklistArray);
 
 
 
+function removeSelectedTrack() {
+    let trackInfo = document.querySelector(".selectedTrack");
+    trackInfo.classList.remove("selectedTrack");
+    
+    let trackImage = document.querySelector(".selectedTrackImage")
+    trackImage.classList.remove("selectedTrackImage");
+    
+    let playImage = document.querySelector(".playImage");
+    trackImage.removeChild(playImage);
+  
+}
 
 
+function createTrackPlayer(track) {
+    let body = document.querySelector("body");
+    let trackPlayback = document.createElement("div");
+    trackPlayback.classList.add("track-playback");
+    body.appendChild(trackPlayback);
+    
+    let trackCaptions = document.createElement("div");
+    trackCaptions.classList.add("track-captions");
+    trackPlayback.appendChild(trackCaptions);
 
+    let audioImageLink = document.createElement("div");
+    audioImageLink.classList.add("audio-image");
+    let audioImage = document.createElement("img");
+    audioImage.src = track.image;
+    audioImage.classList.add("audio-image-link");
+    trackCaptions.appendChild(audioImageLink);
+    audioImageLink.appendChild(audioImage);
 
+    let trackNames = document.createElement("div");
+    trackNames.classList.add("track-names");
+    let trackName = document.createElement("div");
+    trackName.textContent = track.title;
+    trackName.classList.add("track-name");
+    let trackCreator = document.createElement("div");
+    trackCreator.textContent = "endribaku";
+    trackCreator.classList.add("track-creator");
+    trackCaptions.appendChild(trackNames);
+    trackNames.appendChild(trackName);
+    trackNames.appendChild(trackCreator);
+
+    let trackAudio = document.createElement("div");
+    trackAudio.classList.add("track-audio");
+    trackPlayback.appendChild(trackAudio);
+
+    let audioButtons = document.createElement("div");
+    audioButtons.classList.add("audio-buttons");
+    trackAudio.appendChild(audioButtons);
+    
+    //// all buttons
+        let repeat = document.createElement("button");
+        repeat.classList.add("repeat");
+        let repeatImage = document.createElement("img");
+        repeatImage.src = "../images/repeating.svg";
+        audioButtons.appendChild(repeat);
+        repeat.appendChild(repeatImage);
+
+        let skipBack = document.createElement("button");
+        skipBack.classList.add("skip-back");
+        let skipBackImage = document.createElement("img");
+        skipBackImage.src = "../images/skip-back.svg"
+        audioButtons.appendChild(skipBack);
+        skipBack.appendChild(skipBackImage);
+
+        let pause = document.createElement("button");
+        pause.classList.add("pause");
+        let pauseImage = document.createElement("img");
+        pauseImage.src = "../images/pause.svg";
+        audioButtons.appendChild(pause);
+        pause.appendChild(pauseImage);
+
+        let skipForward = document.createElement("button");
+        skipForward.classList.add("skip-forward");
+        let skipForwardImage = document.createElement("img");
+        skipForwardImage.src = "../images/skip-forward.svg";
+        audioButtons.appendChild(skipForward);
+        skipForward.appendChild(skipForwardImage);
+
+        let shuffling = document.createElement("button");
+        shuffling.classList.add("shuffling");
+        let shufflingImage = document.createElement("img");
+        shufflingImage.src = "../images/shuffling.svg";
+        audioButtons.appendChild(shuffling);
+        shuffling.appendChild(shufflingImage);
+    
+    let trackLength = document.createElement("div");
+    let trackAudioLength = parseInt(track.audio.duration)
+    trackLength.classList.add("track-timelength");
+    trackAudio.appendChild(trackLength);
+        let trackMinutes = parseInt(trackAudioLength / 60);
+        let trackSeconds = trackAudioLength % 60;
+        
+        if(trackSeconds < 10) {
+            trackSeconds = "0" + trackSeconds;
+        }
+        let currentTime = document.createElement("div");
+        currentTime.classList.add("current-time");
+        currentTime.textContent = "0:00";
+        trackLength.appendChild(currentTime);
+        
+        let timeSlider = document.createElement("input");
+        timeSlider.classList.add("time-slider")
+        timeSlider.type = "range";
+        timeSlider.min = "0";
+        timeSlider.max = track.audio.duration;
+        timeSlider.step =  "0.1";
+        trackLength.appendChild(timeSlider);
+        
+        let trackTime = document.createElement("div");
+        trackTime.classList.add("track-time");
+        trackTime.textContent = `${trackMinutes}:${trackSeconds}`;
+        trackLength.appendChild(trackTime);
+
+        
+        let audioCustomization = document.createElement("div");
+        trackPlayback.appendChild(audioCustomization);
+
+        let volumeSliderImage = document.createElement("img");
+        volumeSliderImage.src = "../images/volume.png";
+        audioCustomization.appendChild(volumeSliderImage);
+        
+        let volumeSlider = document.createElement("input");
+        volumeSlider.classList.add("volume-slider");
+        volumeSlider.type = "range";
+        volumeSlider.min = "0";
+        volumeSlider.max = "100";
+        audioCustomization.appendChild(volumeSlider);
+};
 
 
 
@@ -430,7 +595,11 @@ function searchBarFilter() {
             } else {
                 yes = false;
                 break;
-            }
+            }    
+        }
+        //if search bar is empty
+        if (word.length == 0) {
+            yes = true;
         }
         return yes;
     });
@@ -442,9 +611,7 @@ function searchBarFilter() {
     
 }
 
-function createTrackPlayer() {
 
-};
 
 
 
@@ -468,6 +635,11 @@ bpm_filter.addEventListener("click", addBpmRange);
 
 let search_bar = document.querySelector(".search-bar");
 search_bar.addEventListener("change", searchBarFilter);
+
+
+
+// function calls
+createTracklist(tracklistArray);
 
 
 
